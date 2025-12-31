7dc8d019942a00ee4ce58be4c223c20fee8bc93f
stooky <chris@fossenier.com>
2025-12-31 09:53:18 -0600

    Use direct IP for production deploy (Cloudflare blocks SSH)

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -20,7 +20,7 @@ const ENVIRONMENTS = {
   },
   production: {
     name: 'Production',
-    host: 'ccansam.com',
+    host: '144.202.24.79',
     user: 'root',
     path: '/var/www/ccan',
     url: 'https://ccansam.com',
